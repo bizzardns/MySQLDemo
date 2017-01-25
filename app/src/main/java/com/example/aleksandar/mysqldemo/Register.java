b06b12d5dbf4b0c054642809afb80ac1a8decae5
Aleksandar <sale_lecar@yahoo.com>
2017-01-25 21:38:45 +0100

    Initial commit

---
+++ app/src/main/java/com/example/aleksandar/mysqldemo/Register.java
@@ -0,0 +1,33 @@
+package com.example.aleksandar.mysqldemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class Register extends AppCompatActivity {
+EditText name, surname, username, password;
+    String str_name, str_surname, str_username, str_password;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        name = (EditText)findViewById(R.id.et_ime);
+        surname = (EditText)findViewById(R.id.et_prezime);
+        username = (EditText)findViewById(R.id.et_username);
+        password = (EditText)findViewById(R.id.et_password);
+
+    }
+
+    public void OnReg (View view){
+        String str_name = name.getText().toString();
+        String str_surname = surname.getText().toString();
+        String str_username = username.getText().toString();
+        String str_password = password.getText().toString();
+        String type = "register";
+        BackgroundWorker backgroundWorker = new BackgroundWorker(this);
+        backgroundWorker.execute(type, str_name, str_surname,str_username,str_password);
+
+    }
+
+}
