2d1ccba723f6a48e3c6cb4039c8a25c5b4d43689
vlatko1988 <vlatko.popovic@gmail.com>
2017-01-30 22:37:06 +0100

    Cao

--- app/src/main/java/com/example/aleksandar/mysqldemo/SmsActivity.java
+++ app/src/main/java/com/example/aleksandar/mysqldemo/SmsActivity.java
@@ -7,12 +7,10 @@ import android.os.Bundle;
 import android.telephony.SmsManager;
 import android.view.Menu;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 public class SmsActivity extends AppCompatActivity {
-
     EditText sms;
     FloatingActionButton send;
 
@@ -21,49 +19,32 @@ public class SmsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sms);
         setTitle("Posalji obavestenje");
-
         sms= (EditText) findViewById(R.id.messageText);
-        String text = sms.getText().toString();
-
+        final String[] MobNumber = {"0691050988","0691050988","0691050988","0691050988"};
         send = (FloatingActionButton) findViewById(R.id.floatingActionButton2);
-
         send.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String text = sms.getText().toString();
-
-               String[] MobNumber = {"0691050988","0691050988","0691050988","0691050988"};
-
                 for (int i = 0; i < MobNumber.length; i++) {
                     String tempMobileNumber = MobNumber[i];
                     sendSMS(tempMobileNumber, text);
                 }
-
-
-
-               // sendSMS("0691050988",text);
-
             }
         });
-
-
     } public boolean onCreateOptionsMenu(Menu menu){
-
-
-
         menu.add("Rezervisi").setIntent(new Intent(this,Main2Activity.class));
         menu.add("Obrisi rezervaciju").setIntent(new Intent(this,Main3Activity.class));
         menu.add("Broj svadbi").setIntent(new Intent(this,SmsActivity.class));
         menu.add("Dodaj admina").setIntent(new Intent(this, Register.class));
         return true;
-
-
     }
-
     public void sendSMS(String phoneNo, String msg) {
         try {
             SmsManager smsManager = SmsManager.getDefault();
             smsManager.sendTextMessage(phoneNo, null, msg, null, null);
+            Toast.makeText(getApplicationContext(), "Sending...",
+                    Toast.LENGTH_LONG).show();
             Toast.makeText(getApplicationContext(), "Message Sent",
                     Toast.LENGTH_LONG).show();
         } catch (Exception ex) {
