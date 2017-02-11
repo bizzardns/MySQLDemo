4bf5cf754d70c731ab2c89a86b0fe074257af09b
vlatko1988 <vlatko.popovic@gmail.com>
2017-02-11 23:48:50 +0100

    Cao

--- app/src/main/java/com/example/aleksandar/mysqldemo/MenuActivity.java
+++ app/src/main/java/com/example/aleksandar/mysqldemo/MenuActivity.java
@@ -5,6 +5,7 @@ import android.provider.Telephony;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.AnimationUtils;
 
 public class MenuActivity extends AppCompatActivity {
 
@@ -14,34 +15,36 @@ public class MenuActivity extends AppCompatActivity {
         setContentView(R.layout.activity_menu);
 
 
+
+
     }
     public void btn(View v)
-    {
+    {    v.startAnimation(AnimationUtils.loadAnimation(getApplicationContext(), R.anim.image_click));
         Intent intent = new Intent(getApplicationContext(), Main2Activity.class);
         startActivity(intent);
     }
     public void btn5(View v)
-    {
+    {   v.startAnimation(AnimationUtils.loadAnimation(getApplicationContext(), R.anim.image_click));
         Intent intent = new Intent(getApplicationContext(), Main3Activity.class);
         startActivity(intent);
     }
     public void btn2(View v)
-    {
+    {   v.startAnimation(AnimationUtils.loadAnimation(getApplicationContext(), R.anim.image_click));
         Intent intent = new Intent(getApplicationContext(), SlobodniBendovi.class);
         startActivity(intent);
     }
     public void btn1(View v)
-    {
+    {   v.startAnimation(AnimationUtils.loadAnimation(getApplicationContext(), R.anim.image_click));
         Intent intent = new Intent(getApplicationContext(), Counter.class);
         startActivity(intent);
     }
     public void btn3(View v)
-    {
+    {   v.startAnimation(AnimationUtils.loadAnimation(getApplicationContext(), R.anim.image_click));
         Intent intent = new Intent(getApplicationContext(), SmsActivity.class);
         startActivity(intent);
     }
     public void btn4(View v)
-    {
+    {    v.startAnimation(AnimationUtils.loadAnimation(getApplicationContext(), R.anim.image_click));
         Intent intent = new Intent(getApplicationContext(), Register.class);
         startActivity(intent);
     }
