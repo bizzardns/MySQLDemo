21752faecf956c692f6b5a5b7bf0e93f3c09ef8a
vlatko1988 <vlatko.popovic@gmail.com>
2017-04-27 03:35:53 +0200

    Cao

--- app/src/main/java/com/example/aleksandar/mysqldemo/AllReservations.java
+++ app/src/main/java/com/example/aleksandar/mysqldemo/AllReservations.java
@@ -1,5 +1,6 @@
 package com.example.aleksandar.mysqldemo;
 
+import android.app.TabActivity;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -10,21 +11,26 @@ import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.RequiresApi;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.text.Editable;
-import android.text.TextUtils;
-import android.text.TextWatcher;
+import android.support.v7.widget.SearchView;
 import android.util.Log;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
-import android.widget.EditText;
-import android.widget.ListAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TabHost;
+import android.widget.Toast;
+
+
+import com.example.aleksandar.mysqldemo.SearchFragment.PagerAdapter;
 
 import java.util.ArrayList;
-import java.util.Locale;
 
 public class AllReservations extends AppCompatActivity {
 
@@ -32,14 +38,12 @@ public class AllReservations extends AppCompatActivity {
 
 
     ContactDB contactBase;
-    android.widget.SearchView sv;
     ArrayAdapter<String> adapter;
     ArrayAdapter listAdapter;
-EditText editText;
     String[] imeBenda;
-
+    SearchView searchView;
     ListView listView;
-
+    AlertDialog alertDialog;
 
     private DrawerLayout mDrawerLayout;
     private ActionBarDrawerToggle mToggle;
@@ -124,89 +128,233 @@ EditText editText;
             }
         });
 
+        setTitle("");
+
+
+
+
         contactBase = new ContactDB(this, null, 1);
-        listView = (ListView) findViewById(R.id.lv);
+
+
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
+        tabLayout.addTab(tabLayout.newTab().setText("GRAD"));
+        tabLayout.addTab(tabLayout.newTab().setText("LOKAL"));
+        tabLayout.addTab(tabLayout.newTab().setText("IME"));
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+
+
+        final ViewPager viewPager = (ViewPager) findViewById(R.id.pager);
+        viewPager.setCurrentItem(1);
+
+        final PagerAdapter adapter = new PagerAdapter
+                (getSupportFragmentManager(), tabLayout.getTabCount());
+        viewPager.setAdapter(adapter);
+
+
+
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+
+
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+                switch (tab.getPosition()){
+                    case 0:
+                            Tab1();
+                        break;
+                    case 1:
+                        Tab2();
+
+                        break;
+
+                    case 2:
+
+                        Tab3();
+                        break;
+                }
+
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+                switch (tab.getPosition()){
+                    case 0:
+                        Tab1();
+                        break;
+                    case 1:
+                        Tab2();
+
+                        break;
+
+                    case 2:
+
+                        Tab3();
+                        break;
+                }
+            }
+        });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        /*contactBase = new ContactDB(this, null, 1);
+        listView = (ListView) findViewById(R.id.lv1);
         ArrayList<String> theList = new ArrayList<>();
         Cursor cursor = contactBase.list_all_list2();
-editText = (EditText) findViewById(R.id.editText);
+
         while (cursor.moveToNext()) {
 
 
-            theList.add(cursor.getString(3) + " " +cursor.getString(1));
+            theList.add(cursor.getString(4) + "\n" +cursor.getString(1) +"\n"+ cursor.getString(2)+"\n"+ cursor.getString(5));
 
             imeBenda = theList.toArray(new String[0]);
 
         }
+
         //listAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, imeBenda);
+        listAdapter = new ArrayAdapter<String>(this, R.layout.liste, R.id.bendTxt, imeBenda);
+        listView.setAdapter(listAdapter);*/
+
+
+
+
+    }
+
+
+    public void Tab1 (){
+        listView = (ListView) findViewById(R.id.lv1);
+        ArrayList<String> theList = new ArrayList<>();
+        Cursor cursor = contactBase.list_all_list2();
+
+        while (cursor.moveToNext()) {
+
+
+            theList.add(cursor.getString(4) + "\n" +cursor.getString(1) +"\n"+ cursor.getString(2)+"\n"+ cursor.getString(5)+"  " +cursor.getString(6));
+
+            imeBenda = theList.toArray(new String[0]);
+
+        }
+
+        // listAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, imeBenda);
         listAdapter = new ArrayAdapter<String>(this, R.layout.liste, R.id.bendTxt, imeBenda);
         listView.setAdapter(listAdapter);
 
-       // listView.setFilterText(cursor.getString(2));
-        editText.addTextChangedListener(new TextWatcher() {
+    }
+    public void Tab2 (){
+        listView = (ListView) findViewById(R.id.lv2);
+        ArrayList<String> theList = new ArrayList<>();
+        Cursor cursor = contactBase.list_all_list2();
+
+        while (cursor.moveToNext()) {
+
+
+            theList.add(cursor.getString(5) + "\n" +cursor.getString(1) +"\n"+ cursor.getString(2)+"\n"+ cursor.getString(4)+"  " +cursor.getString(6));
+
+            imeBenda = theList.toArray(new String[0]);
+
+        }
+
+        // listAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, imeBenda);
+        listAdapter = new ArrayAdapter<String>(this, R.layout.liste, R.id.bendTxt, imeBenda);
+        listView.setAdapter(listAdapter);
+
+    }
+    public void Tab3 (){
+        listView = (ListView) findViewById(R.id.lv3);
+        ArrayList<String> theList = new ArrayList<>();
+        Cursor cursor = contactBase.list_all_list2();
+
+        while (cursor.moveToNext()) {
+
+
+            theList.add(cursor.getString(3) + "\n" +cursor.getString(1) +"\n"+ cursor.getString(2)+"\n"+ cursor.getString(5)+"  " +cursor.getString(6));
+
+            imeBenda = theList.toArray(new String[0]);
+
+        }
+
+        // listAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, imeBenda);
+        listAdapter = new ArrayAdapter<String>(this, R.layout.liste, R.id.bendTxt, imeBenda);
+        listView.setAdapter(listAdapter);
+
+    }
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu( Menu menu) {
+        getMenuInflater().inflate( R.menu.menu_main2, menu);
+
+        MenuItem myActionMenuItem = menu.findItem( R.id.action_search);
+        searchView = (SearchView) myActionMenuItem.getActionView();
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            public boolean onQueryTextSubmit(String query) {
 
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-
-               AllReservations.this.listAdapter.getFilter().filter(s.toString());
-                //listView.setTextFilterEnabled(true);
-               // listView.setFilterText(s.toString());
-                //listView.setFilterText(s.toString().trim());
-
-                listView.setFilterText(s.toString());
-                listView.setFilterText(s.toString());
-
-
-
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-                AllReservations.this.listAdapter.getFilter().filter(s);
-                listView.setFilterText(s.toString());
-                listView.setFilterText(s.toString());
-            }
-                });
-
-
-
-        /*adapter1 = new OfflineListLogic(AllReservations.this, imeBenda, ime, grad, restoran,event);
-        listView.setAdapter(adapter1);*/
-
-
-
-
-
-       /*sv = (android.widget.SearchView) findViewById(R.id.sv);
-
-        sv.setOnQueryTextListener(new android.widget.SearchView.OnQueryTextListener() {
-            @Override
-            public boolean onQueryTextSubmit(String text) {
                 return false;
             }
-
             @Override
-            public boolean onQueryTextChange(String text) {
-                for(int i = 0; i < l.getAllListViewItems().size(); i++){
-                    if (TextUtils.isEmpty(text)) {
-                        filter.filter(null);
-                    } else {
-                        String itemName = ChainageFactory.getAllListViewItems().YourItem();
+            public boolean onQueryTextChange(String s) {
+                if (listAdapter == null){
+                    alertDialog = new AlertDialog.Builder(AllReservations.this).create();
 
-                        if(itemName.contains(text)){
-                            //Perform your operation with filtered data
-                        }
-                    }
+                    alertDialog.setMessage("Izaberite kategoriju pretrage!");
+                    alertDialog.setButton(AlertDialog.BUTTON_POSITIVE, "OK",
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int which) {
+                                    dialog.dismiss();
+                                }
+                            });
+                    alertDialog.show();
+                    //Toast.makeText(AllReservations.this, "", Toast.LENGTH_SHORT).show();
+
+                }else{
+
+                    AllReservations.this.listAdapter.getFilter().filter(s);
+
                 }
-                return true;
+
+
+                return false;
             }
-        });*/
-        //OfflineMode.ListUtils.setDynamicHeight(listView);
+        });
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (mToggle.onOptionsItemSelected(item)) {
+
+            return true;
+
+        }
+        int id = item.getItemId();
 
 
+
+
+
+        return super.onOptionsItemSelected(item);
     }
 
 
